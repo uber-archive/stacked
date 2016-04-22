cf8cabf9e21557e220f2a0b9cd287bb7ac6c6cdd
Joshua T Corbin <joshua@uber.com>
2016-04-21 17:18:26 -0700

    Add ListenServer(Handler)

---
+++ listen_server.go
@@ -0,0 +1,66 @@
+package stacked
+
+import (
+	"bufio"
+	"net"
+)
+
+// ListenServer is the minimal downstream server interface, e.g. implemented by
+// http.Server.
+type ListenServer interface {
+	Serve(ln net.Listener) error
+}
+
+// ListenServerHandler creates a compatibility Handler for
+func ListenServerHandler(srv ListenServer) Handler {
+	return &connBufShim{Server: srv}
+}
+
+// connBufShim implements Handler interface around a ListenServer
+type connBufShim struct {
+	Server    ListenServer
+	listeners map[net.Addr]*bufListener
+}
+
+// ServeConnection simply puts a new bufConn onto bufConns for distribution by
+// bufLn.Accept.
+func (cbs *connBufShim) ServeConnection(conn net.Conn, bufr *bufio.Reader) {
+
+	ln := cbs.lnFor(conn)
+
+	ln.conns <- &bufConn{conn, bufr}
+}
+
+// lnFor gets or creates the bufListener for connection (one-per
+// conn.LocalAddr()).
+func (cbs *connBufShim) lnFor(conn net.Conn) *bufListener {
+	addr := conn.LocalAddr()
+	if cbs.listeners == nil {
+		cbs.listeners = make(map[net.Addr]*bufListener, 1)
+	}
+	if ln := cbs.listeners[addr]; ln != nil {
+		return ln
+	}
+	ln := &bufListener{
+		addr:  addr,
+		conns: make(chan *bufConn),
+	}
+	cbs.listeners[addr] = ln
+	go func() {
+		if err := cbs.Server.Serve(ln); err != nil {
+			delete(cbs.listeners, addr)
+		}
+	}()
+	return ln
+}
+
+// Close closes any bufListeners
+func (cbs *connBufShim) Close() error {
+	if cbs.listeners == nil {
+		for _, ln := range cbs.listeners {
+			ln.Close() // TODO: care about error? use a MultiError?
+		}
+		cbs.listeners = nil
+	}
+	return nil
+}
