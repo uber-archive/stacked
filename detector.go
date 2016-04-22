bbc0290f733fd8c140a1b58b32096b66ceb166e0
Joshua T Corbin <joshua@uber.com>
2016-04-21 17:21:37 -0700

    Add detector module

---
+++ detector.go
@@ -0,0 +1,37 @@
+package stacked
+
+import "net/http"
+
+// Detector is a Handler along with a detection function and how
+// many bytes it needs to decide.
+type Detector struct {
+	// Needed is how many bytes are needed for the Test function
+	Needed int
+
+	// Test will be called with at least Needed bytes.  If this function
+	// returns true, then the connection will be given to Handler.
+	Test func(b []byte) bool
+
+	Handler Handler
+}
+
+// DefaultHTTPHandler creates a FallthroughDetector around an http.Handler.
+func DefaultHTTPHandler(hndl http.Handler) Detector {
+	if hndl == nil {
+		hndl = http.DefaultServeMux
+	}
+	handler := ListenServerHandler(&http.Server{
+		Handler: hndl,
+	})
+	return FallthroughDetector(handler)
+}
+
+// FallthroughDetector returns a Detector whose Test function always returns
+// true.  No bytes are needed for tautology.
+func FallthroughDetector(hndl Handler) Detector {
+	return Detector{
+		Needed:  0,
+		Test:    func([]byte) bool { return true },
+		Handler: hndl,
+	}
+}
